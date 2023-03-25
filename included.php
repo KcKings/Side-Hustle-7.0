00f2efc8857f8cabbb8ebc1c6580a9afed9b4cd6
Kelechi Alexander <kelechialexo@gmail.com>
2023-03-25 16:29:00 +0100

    Great work not complete yet

---
+++ included.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Welcome to the week";
+
+    $connection = mysqli_connect("localhost", "root", "", "sidehustle.db");
+?>
---
+++ sidehustledatabase.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DataBase Link And Use</title>
+</head>
+<body>
+    <?php
+        include 'included.php'
+    ?>
+    <p>Let's get started with Connecting our page to our DataBase</p>
+    <?php
+        if ($connection) {
+            echo "Connected to the Database successfully";
+        }else{
+        echo "Unable to connect";
+    }
+
+    $put_data = mysqli_query($connection, "INSERT INTO guest-profile(Name) VALUES('Dan')");
+
+        if($put_data){
+            echo "data saved successfully";
+        }else{
+            echo "We are not There yet:";
+        }
+    ?>
+    
+</body>
+</html>
