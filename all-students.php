a4cf2c40dae889b038d7bdea404e40ac242bb55b
Kelechi Alexander <kelechialexo@gmail.com>
2023-04-04 16:55:13 +0100

    Updated the file names

--- all-students.php
+++ all-students.php
@@ -1,38 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Students</title>
 </head>
+
 <body>
     <?php
     require "included.php";
-    
     $students = mysqli_query($connection, 'SELECT * FROM  students');
     ?>
     <a href="./createstudent.php">Add New Student</a>
+    <br><br>
     <h2>All Students</h2>
     <table>
         <thead>
-            <td>S/N</td>
-            <td>Name</td>
-            <td>Age</td>
-            <td>Gender</td>
-            <td>Actions</td>
+            <th>S/N</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Actions</th>
         </thead>
         <tbody>
-            <?php foreach($students as $student): ?>
+            <?php foreach ($students as $student) : ?>
                 <tr>
-                    <td><?= $student['id']?></td>
-                    <td><?= $student['name']?></td>
-                    <td><?= $student['age']?></td>
-                    <td><?= $student['gender']?></td>
+                    <td><?= $student['id'] ?></td>
+                    <td><?= $student['name'] ?></td>
+                    <td><?= $student['age'] ?></td>
+                    <td><?= $student['gender'] ?></td>
                     <td>
-                        <a href="view-student.php?id=<? $student['id'] ?>">View</a>
-                        <a href="edit-student.php?id=<? $student['id'] ?>">Edit</a>
-                        <a href="delete-student.php?id=<? $student['id'] ?>">Delete</a>
+                        <a href="view-student.php?id=<?= $student['id'] ?>">View</a>
+                        <a href="edit-student.php?id=<?= $student['id'] ?>">Edit</a>
+                        <a href="delete-student.php?id=<?= $student['id'] ?>">Delete</a>
                     </td>
                 </tr>
             <?php endforeach; ?>
@@ -41,4 +43,5 @@
     </table>
 
 </body>
+
 </html>
--- view-student.php
+++ view-student.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>STUDENT VIEW</title>
 </head>
+
 <body>
+    <a href="/all-students.php">All Students</a>
     <?php
-        include 'included.php';
-        
-        $id = $_GET['id'];
-        $view = mysqli_query($connection, "SELECT * FROM students WHERE id = '$id'");
+    include 'included.php';
 
-
-
+    $id = $_GET['id'];
+    $student = mysqli_query($connection, "SELECT * FROM students WHERE id = '$id'");
+    $student_details = mysqli_fetch_assoc($student);
+    if (!$student_details) {
+        exit('Student Not Found');
+    }
     ?>
+    <h2>Student Details</h2>
+    <p>Name: <?= $student_details['name'] ?></p>
+    <p>Age: <?= $student_details['age'] ?></p>
+    <p>Gender: <?= $student_details['gender'] ?></p>
+
 </body>
+
 </html>
